cb8ae682a2f54317cda22f68014501fd1ad1b394
Omar Essam (Inpu) <o.essameldin@gmail.com>
2019-12-15 19:24:52 +0100

    Add filters to ReadAll

--- app/pkg/database/types.go
+++ app/pkg/database/types.go
@@ -1,25 +1,55 @@
 package database
 
+// ValidationError Validation error struct
 type ValidationError struct {
 	Field   string
 	Message string
 }
 
+// Validator struct to link  validation error with field
 type Validator struct {
 	Field string
 	Error error
 }
 
+// Updated struct for values to be updated
 type Updated struct {
 	Key string
 	Val interface{}
 }
 
+// Operator enum to determine filter operator
+type Operator string
+
+const (
+	// Equals Operator
+	Equals Operator = "=="
+
+	// NotEquals Operator
+	NotEquals string = "!="
+	// MoreThan Operator
+	MoreThan = ">"
+	// LessThan Operator
+	LessThan = "<"
+	// MoreThanOrEqual Operator
+	MoreThanOrEqual = ">="
+	// LessThanOrEqual Operator
+	LessThanOrEqual = "<="
+)
+
+// Filter struct for filtering queries
+type Filter struct {
+	Operator Operator
+	Field    string
+	Value    interface{}
+}
+
+// Connector database adapter interface
 type Connector interface {
 	CloseConnection() error
 	Create(validators []Validator, key string, data interface{}) error
 	Update(validators []Validator, key string, data []Updated) error
 	Read(key string, model interface{}) error
-	ReadAll(genRefFn func() interface{}, appendFn func(interface{})) error
+	ReadAll(genRefFn func() interface{}, appendFn func(interface{}), filters []Filter) error
 	Delete(key string) error
 }
